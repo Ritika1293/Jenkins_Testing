e695ea073df13a320e6f3261e59a7005c065687b
HP <HP@DESKTOP-JE6E4BM>
2025-06-14 11:28:14 +0530

    First commit

---
+++ Jenkins_Execution/src/test/java/Jenkins_Automation/JenkinsTest.java
@@ -0,0 +1,14 @@
+package Jenkins_Automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	@Test
+	public void launch() {
+		WebDriver driver = new ChromeDriver();
+		Reporter.log("Empty browser launched in JenkinsTest class", true);				
+	}
+}
